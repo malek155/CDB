b471d4d9af930bda677d07dceb44e802a9ee7728
sarra chebbi <sarrachebbi1997@gmail.com>
2020-11-21 19:53:33 +0100

    caches class implements interface cache for method tried some methods in echoLogic

--- src/main/java/de/tum/i13/server/echo/EchoLogic.java
+++ src/main/java/de/tum/i13/server/echo/EchoLogic.java
@@ -1,21 +1,113 @@
 package de.tum.i13.server.echo;
 
+import de.tum.i13.server.kv.KVStoreProcessor;
 import de.tum.i13.shared.CommandProcessor;
 
-import java.net.InetAddress;
-import java.net.InetSocketAddress;
+import java.util.Collections;
 import java.util.logging.Logger;
+import java.io.IOException;
+import java.net.*;
 
 public class EchoLogic implements CommandProcessor {
     public static Logger logger = Logger.getLogger(EchoLogic.class.getName());
+    KVStoreProcessor kvp = new KVStoreProcessor();
+    public static ServerSocket serverSocket;
 
+
+    // Sarra: implementation of command line parameters
     public String process(String command) {
 
         logger.info("received command: " + command.trim());
 
-        //Let the magic happen here
+        // Let the magic happen here
 
+        switch (command.trim().substring(0, 2)) {
+            case "-p":
+                setServerPort(Integer.parseInt(command.trim().substring(2)));
+                break;
+            case "-a":
+                setServerAddress(command.trim().substring(2));
+                break;
+            case "-b":
+                ;
+                break;
+            case "-d":
+                ;
+                break;
+            case "-l":
+                ;
+                break;
+            case "-c":
+                kvp.getCache().setSize(Integer.parseInt(command.trim().substring(2)));
+                break;
+            case "-s":
+                setCacheDispalcement(command.trim().substring(2));
+                break;
+            case "-h":
+                printHelp();
+                break;
+        }
         return command;
+    }
+
+    /**
+     *
+     */
+    public void setServerPort(int port) {
+        try {
+            serverSocket = new ServerSocket(port);
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+            System.out.println("Host is unknown");
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.out.println("Error");
+        }
+    }
+
+
+    public void setServerAddress(String IPaddress) {
+        //the second argument is the backlog= max number of clients
+        try {
+            serverSocket = new ServerSocket(0, 1000, InetAddress.getByName(IPaddress));
+        } catch (IOException e) {
+            try {
+                serverSocket = new ServerSocket(0, 1000, InetAddress.getByName("127.0.0.1"));
+            } catch (IOException ioException) {
+                ioException.printStackTrace();
+                System.out.println("IP Address not recognized");
+            }
+
+        }
+    }
+// i gave up on this one
+    public void setCacheDispalcement(String strategy) {
+        if (strategy.toLowerCase().equals("fifo")||strategy.toLowerCase().equals("lru")){
+
+        }
+        else if (strategy.toLowerCase().equals("lfu")){
+
+        }
+        else return;
+    }
+
+    public void printHelp() {
+        System.out.println("Available commands:");
+        System.out.println("connect <address> <port> - Tries to establish a TCP- connection to the echo server based on the given server address and the port number of the echo service.");
+        System.out.println("disconnect - Tries to disconnect from the connected server.");
+        System.out.println("send <message> - Sends a text message to the echo server according to the communication protocol.");
+        System.out.println("logLevel <level> - Sets the logger to the specified log level (ALL | DEBUG | INFO | WARN | ERROR | FATAL | OFF)");
+        System.out.println("help - Display this help");
+        System.out.println("quit - Tears down the active connection to the server and exits the program execution.");
+    }
+
+    //public int cacheSize() {
+    //   return kvp.getCache().getSize();
+    //}
+
+    public static void main(String[] args) {
+        logger.setLevel(null);
+        System.out.println(logger.getName());
     }
 
     @Override
--- src/main/java/de/tum/i13/server/kv/KVCommandProcessor.java
+++ src/main/java/de/tum/i13/server/kv/KVCommandProcessor.java
@@ -6,19 +6,22 @@ import java.net.InetAddress;
 import java.net.InetSocketAddress;
 
 public class KVCommandProcessor implements CommandProcessor {
-    final private KVStore kvStore;
+    private KVStore kvStore;
+    private Cache cache;
+
+    public void setCacheStrategy(Cache cache){
+        if (cache.getClass().equals(LFUCache.class))
+            this.cache = (LFUCache) cache;
+        else this.cache = (FIFOLRUCache) cache;
+    }
 
     public KVCommandProcessor(KVStore kvStore) {
         this.kvStore = kvStore;
     }
 
-
-    @Override
     public String process(String command) {
-        //TODO
         int keyDelimiter = 0;
         if (command.substring(0, 3).toLowerCase() == "put")
-            //Parse message "put message", call kvstore.put
             keyDelimiter = command.substring(4).indexOf(' ');
         try {
             this.kvStore.put(command.substring(4, keyDelimiter), command.substring(keyDelimiter + 1));
@@ -27,10 +30,15 @@ public class KVCommandProcessor implements CommandProcessor {
         }
         if (command.substring(0, 3).toLowerCase() == "get") {
             keyDelimiter = command.substring(4).indexOf(' ');
-            try {
-                this.kvStore.get(command.substring(4));
-            } catch (Exception e) {
-                e.printStackTrace();
+            if (cache.get(command.substring(4))==null) {
+                try {
+                    this.kvStore.get(command.substring(4));
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+            else{
+
             }
         } else {
 
