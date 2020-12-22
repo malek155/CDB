271e03262302982069a007ee03d4a38235bc1f31
ga62juq <ga62juq@mytum.de>
2020-12-22 21:37:18 +0100

    milestone1main updates from malek

--- src/main/java/de/tum/i13/client/Milestone1Main.java
+++ src/main/java/de/tum/i13/client/Milestone1Main.java
@@ -114,6 +114,7 @@ public class Milestone1Main {
 										new Metadata(ipAndPort[0], Integer.parseInt(ipAndPort[1]), entry2[0], entry2[1]));
 
 							});
+							metadataMap = metadataMap2;
 							Metadata meta = null;
 							try {
 								// getting the server which is responsible of this key
@@ -278,7 +279,6 @@ public class Milestone1Main {
 
 				}
 			}
-
 		}
 		return result;
 
